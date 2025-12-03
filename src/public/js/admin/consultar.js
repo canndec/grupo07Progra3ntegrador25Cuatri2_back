f8f33e4a0c841fe119b7481fcde6e73216424cab
canndec <candecorral22@gmail.com>
2025-12-03 20:28:11 -0300

    ultimos detalles minimos

--- src/public/js/admin/consultar.js
+++ src/public/js/admin/consultar.js
@@ -24,28 +24,3 @@ formulario.addEventListener("submit", async (event) => {
     }
 
 });
-/*
-function mostrarProducto(productoCoincidente, datos) {
-    let htmlProductos = "";
-    htmlProductos = ` 
-    <div class="cartaUnProducto">
-            <img class="productoImagen"src="${productoCoincidente.imagen}" alt="${productoCoincidente.nombre}">
-            <h3>${productoCoincidente.nombre}</h3>
-            <p>$${productoCoincidente.precio}</p>
-            <p> id: ${productoCoincidente.id}</p>  
-            <p> ${hayStock(productoCoincidente.activo)}</p>
-        </div>
-    `;
-    gridProductos.innerHTML = htmlProductos;
-}
-function hayStock(activo){
-    return activo == 1 ? `Disponible` : `No hay stock`;
-}
-
-function mostrarError(mensaje) {
-    gridProductos.innerHTML = `
-        <div class="mensajeError">
-            <strong>Error:</strong>
-            <span>${mensaje}</span>
-        </div>`;
-}*/
